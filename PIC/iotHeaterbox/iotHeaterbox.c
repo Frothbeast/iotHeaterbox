4192e36ef95edd1f731a6c9580ae9b9b01dce3f5
Frothbeast <frothbeast@gmail.com>
2026-06-24 20:28:34 -0400

    need to add control and esp and menu changing
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- PIC/iotHeaterbox/iotHeaterbox.c
+++ PIC/iotHeaterbox/iotHeaterbox.c
@@ -40,7 +40,7 @@ volatile float t_h = 0.0;
 volatile float t_b = 0.0;
 volatile uint8_t adc_ready = 0;
 volatile uint8_t isr_channel = 0;
-volatile float box_setpoint = 25.0;
+volatile uint8_t box_setpoint = 45.0;
 volatile float heater_target = 0.0;
 volatile uint16_t adc_val[2];
 volatile uint8_t flag_10hz = 0;
@@ -53,21 +53,22 @@ uint8_t last_menu_state = 0xFF;
 volatile uint8_t menu_press = 0, up_press = 0, down_press = 0, select_press = 0;
 volatile uint8_t btn_menu = 0, btn_up = 0, btn_down = 0, btn_select = 0;
 volatile uint8_t btn_menu_state = 0, btn_up_state = 0, btn_down_state = 0, btn_select_state = 0;
+
 #define not_pressed 0
 #define was_pressed 1
 #define being_held  3
 
 #define main_menu 0
-#define setpoint_menu 1
-#define wifi_menu 2
-#define manual_menu 3
+#define setpoint_menu 2
+#define wifi_menu 3
+#define manual_menu 1
 #define pid_menu 4
-
 
 volatile uint8_t *btn_pins[] = {&btn_menu, &btn_up, &btn_down, &btn_select};
 volatile uint8_t *btn_states[] = {&btn_menu_state, &btn_up_state, &btn_down_state, &btn_select_state};
 volatile uint8_t *btn_flags[] = {&menu_press, &up_press, &down_press, &select_press};
-volatile uint8_t time_to_display = 0;
+volatile uint8_t time_to_send = 0;
+
 uint8_t lcd_lines[] = {LCD_L1, LCD_L2, LCD_L3, LCD_L4};
 uint8_t current_line_idx = 0;
 char display_buffer[4][21]; // 4 lines, 20 chars + null terminator
@@ -75,9 +76,7 @@ char display_buffer[4][21]; // 4 lines, 20 chars + null terminator
 float Kp = 10.0, Ki = 0.5, Kd = 0.1;
 uint8_t fan_mode = 0;
 uint8_t control_active = 1;
-int8_t mock_rssi = -65;
 
-char s[24];
 
 void soft_putch(char data) {
     uint8_t gie_backup = INTCONbits.GIE;
@@ -106,13 +105,6 @@ void lcd_move_cursor(uint8_t address) {
 }
 
 void lcd_write(const char *str) { while(*str) soft_putch(*str++); }
-
-uint8_t get_button_states(void) {
-    // Read PORTB, mask bits 0-3
-    // Since buttons are active-low (pressed = 0), 
-    // invert the result with ~ to make pressed = 1
-    return (~PORTB) & 0x0F;
-}
 
 void poll_buttons(void) {
     btn_menu = (PORTBbits.RB3 == 0);
@@ -190,12 +182,111 @@ void __interrupt() ISR(void) {
         flag_10hz = 1;
         INTCONbits.TMR0IF = 0;
     }
+    if (PIR1bits.TMR1IF) {
+        static uint16_t timer1_counter = 0;
+        timer1_counter++;
+        if (timer1_counter >= 572) {
+            time_to_send = 1;
+            timer1_counter = 0;
+        }
+    
+        PIR1bits.TMR1IF = 0;
+    }
 }
 
 float calc_celsius(uint16_t adc, float fixed_res, uint8_t type) {
     if (adc <= 5) adc = 5; if (adc >= 1020) adc = 1020;
     float resistance = (type == 0) ? (fixed_res * ((float)adc / (1023.0 - (float)adc))) : (fixed_res * ((1023.0 - (float)adc) / (float)adc));
     return (1.0 / (1.0 / 298.15 + (1.0 / 4350.0) * log(resistance / 100000.0))) - 273.15;
+}
+
+void handle_buttons(void){
+    if (menu_press){
+        menu_press=0;
+        menu_state++;
+        if (menu_state > pid_menu) {
+            menu_state = main_menu;
+        }
+        lcd_cmd_direct(LCD_CLR);
+        
+        switch (menu_state){
+            case main_menu:
+                sprintf(display_buffer[0], "Main Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+
+                break;
+            case setpoint_menu:
+                sprintf(display_buffer[0], "Setpoint Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+                break;
+            case wifi_menu:
+                sprintf(display_buffer[0], "WIFI Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+                break;
+            case manual_menu:
+                sprintf(display_buffer[0], "Manual Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+                break;
+            case pid_menu:
+                sprintf(display_buffer[0], "PID Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+                break;
+            default:
+                sprintf(display_buffer[0], "default Menu");
+                sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+                sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
+                break;
+        }
+    }
+    
+    if (up_press){
+        up_press=0;
+
+    }
+    if (down_press){
+        down_press=0;
+
+    }
+    if (select_press){
+        select_press=0;
+
+}
+}
+
+void control_outputs(void){
+    FAN = 1;
+    LIGHT = 1;
+    if (t_b<= box_setpoint){
+        HEATER = 1;
+    }
+    else {
+        HEATER = 0;
+    }
+}
+
+void send_to_esp(void) {
+    
+}
+
+void send_to_display(void) {
+    
+    uint8_t old_gie = INTCONbits.GIE; //save state of interrupts
+    INTCONbits.GIE = 0;// stop all interrupts
+
+    lcd_move_cursor(lcd_line_addrs[current_line_idx]);
+    lcd_write(display_buffer[current_line_idx]); 
+
+    current_line_idx++;
+    if (current_line_idx >= 4) {
+        current_line_idx = 0;
+    } 
+
+    INTCONbits.GIE = old_gie;// restore interrupts
 }
 
 void main(void) {
@@ -221,8 +312,8 @@ void main(void) {
     t_h = calc_celsius(adc_val[0], 698.0, 1);
     t_b = calc_celsius(adc_val[1], 50300.0, 0);
     
-    // Splash screen (standard delay allowed here as no other tasks are running)
-    __delay_ms(2000);
+    lcd_cmd_direct(LCD_ON_NO_CURSOR);
+    lcd_cmd_direct(LCD_BACKLIGHT_ON);
     lcd_cmd_direct(LCD_CLR);
     
     // Initial buffer setup
@@ -232,50 +323,35 @@ void main(void) {
     sprintf(display_buffer[3], "from big toe surgery");
     
     lcd_move_cursor(lcd_line_addrs[0]);
-    lcd_write(display_buffer[LCD_L1]);
+    lcd_write(display_buffer[0]);
     lcd_move_cursor(lcd_line_addrs[1]);
-    lcd_write(display_buffer[LCD_L2]);
+    lcd_write(display_buffer[1]);
     lcd_move_cursor(lcd_line_addrs[2]);
-    lcd_write(display_buffer[LCD_L3]);
+    lcd_write(display_buffer[2]);
     lcd_move_cursor(lcd_line_addrs[3]);
-    lcd_write(display_buffer[LCD_L4]);
+    lcd_write(display_buffer[3]);
+    
+          // Splash screen (standard delay allowed here as no other tasks are running)
+    __delay_ms(2000);
+    lcd_cmd_direct(LCD_CLR);
 
     // Start interrupts for main loop
     T0CON = 0x84; INTCONbits.TMR0IE = 1; PIE1bits.ADIE = 1; INTCONbits.GIE = 1; INTCONbits.PEIE = 1;
+    T1CON = 0x30; TMR1H = 0; TMR1L = 0; PIE1bits.TMR1IE = 1; T1CONbits.TMR1ON = 1;
+    
     ADCON0bits.CHS = 0; 
     ADCON0bits.GO = 1;
-    lcd_cmd_direct(LCD_ON_NO_CURSOR);
-    lcd_cmd_direct(LCD_BACKLIGHT_ON);
-    time_to_display = 1;
     
     sprintf(display_buffer[0], "Main Menu");
     sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
     sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-
+    sprintf(display_buffer[3], "");
 
     while(1) {
         
-        if (time_to_display){
-            time_to_display=0;
-            
-            uint8_t old_gie = INTCONbits.GIE; //save state of interrupts
-            INTCONbits.GIE = 0;// stop all interrupts
-            
-            lcd_move_cursor(lcd_line_addrs[current_line_idx]);
-            lcd_write(display_buffer[current_line_idx]); 
-
-            current_line_idx++;
-            if (current_line_idx >= 4) {
-                current_line_idx = 0;
-            } 
-            
-            INTCONbits.GIE = old_gie;// restore interrupts
-        }
-        
         if (flag_10hz) {
             flag_10hz = 0;
-            time_to_display=1;
-            //// get a/d values when they are ready
+            
             if (adc_ready) {
                 adc_ready = 0;
                 if (!isr_channel) {
@@ -285,63 +361,22 @@ void main(void) {
                     t_b = calc_celsius(adc_val[1], 50300.0, 0);
                 }
             }
+            sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
+            sprintf(display_buffer[2], "F:%d L:%d H:%d SP:%3d", FAN, LIGHT, HEATER, box_setpoint);
+            
+            send_to_display();
             
             poll_buttons();
             
-            if (menu_press){
-                menu_press=0;
-                menu_state++;
-                if (menu_state > pid_menu) {
-                    menu_state = main_menu;
-                }
-                lcd_cmd_direct(LCD_CLR);
-                switch (menu_state){
-                    case main_menu:
-                        sprintf(display_buffer[0], "Main Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        
-                        break;
-                    case setpoint_menu:
-                        sprintf(display_buffer[0], "Setpoint Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        break;
-                    case wifi_menu:
-                        sprintf(display_buffer[0], "WIFI Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        break;
-                    case manual_menu:
-                        sprintf(display_buffer[0], "Manual Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        break;
-                    case pid_menu:
-                        sprintf(display_buffer[0], "PID Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        break;
-                    default:
-                        sprintf(display_buffer[0], "default Menu");
-                        sprintf(display_buffer[1], "H:%5.1f B:%5.1f", t_h, t_b);
-                        sprintf(display_buffer[2], "F:%d L:%d H:%d", FAN, LIGHT, HEATER);
-                        break;
-                        
-                }
+            handle_buttons();
+            
+            control_outputs();
+            
+            if(time_to_send){
+                send_to_esp();
             }
-            if (up_press){
-                up_press=0;
-                
-            }
-            if (down_press){
-                down_press=0;
-                
-            }
-            if (select_press){
-                select_press=0;
-                
-            }
+            
+
         }
     }
 }
